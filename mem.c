b14c984763bc17b5523a9febd92aabbc2f48d6dc
John Morgan Wight <john.morgan.wight@proton.me>
2025-05-29 16:06:58 -0400

    Code from book mostly modified **UNBUILT/UNTESTED*
    Only modification made was that sbrk returns void* now (book has it has
    char*. This must have changed in the standard at some point.

---
+++ mem.c
@@ -0,0 +1,100 @@
+//#include <unistd.h> /* for sbrk */
+#include "mem.h"
+#define _NALLOC	1024
+
+typedef long long Align; /* most restrictive type except for SSE vector which I
+			 * am not worried about */
+
+union header		/* block header: */
+{
+	struct
+	{
+		union header *ptr; /* next block if on free list */
+		unsigned size; /* size of this block */
+	} s;
+	Align x; /* force alignment of blocks for cpu optimization */
+};
+
+typedef union header Header;
+
+static Header base; /* empty list of get started */
+static Header *freep = NULL; /* start of free list */
+
+/* malloc: general-purpose storage allocator */
+void *malloc(unsigned nbytes)
+{
+	Header *p, *prevp;
+	Header *morecore(unsigned);
+	unsigned nunits;
+
+	nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
+	if((prevp = freep) == NULL) /* no free list yet */
+	{
+		base.s.ptr = freep = prevp = &base;
+		base.s.size = 0;
+	}
+	for(p = prevp->s.ptr; ; prevp = p, p = p->s.ptr)
+	{
+		if(p->s.size >= nunit) /* big enough */
+		{
+			if(p->s.size == nunits) /* exactly */
+				prevp->s.ptr = p->s.ptr;
+			else
+			{
+				p->s.size -= nunits;
+				p += p->s.size;
+				p->s.size = nunits;
+			}
+			freep = prevp;
+			return (void *)(p+1);
+		}
+		if(p == freep) /* wrapped around free list */
+			if((p = morecore(nunits)) == NULL)
+				return NULL;
+	}
+}
+
+/* morecore: ask system for more memory */
+static Header *morecore(unsigned nu)
+{
+	void *cp, sbrk(int) /* this is in <unistd.h>, could use that instead */
+	Header *up;
+
+	if(nu < NALLOC)
+		nu = NALLOC;
+	cp = sbrk(nu * sizeof(Header));
+	if(cp == (char *) - 1) /* no space at all */
+		return NULL;
+	up = (Header *) cp;
+	up->s.size = nu;
+	free((void *)(up + 1));
+	return freep;
+}
+
+/* free: put block ap in free list */
+void free(void *ap)
+{
+	Header *bp, *p;
+
+	bp = (Header *)ap - 1; /* points to block header */
+	for(p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
+	{
+		if(p >= p->s.ptr && (bp > p || bp < p->s.ptr))
+			break; /* freed block at start or end of arena */
+	}
+	if(bp + bp->s.size == p->s.ptr) /* join to upper nbr */
+	{
+		bp->s.size += p->s.ptr->s.size;
+		bp->s.ptr = p->s.ptr->s.ptr;
+	}
+	else
+		bp->s.ptr = p->s.ptr;
+	if(p + p->s.ptr = p->s.ptr) /* join to lower nbr */
+	{
+		p->s.size += bp->s.size;
+		p->s.ptr = bp->s.ptr;
+	}
+	else
+		p->s.ptr = bp;
+	freep = p;
+}
